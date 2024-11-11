85b3a80523ae7fe9035edf7b6965d149509c3d0b
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2024-11-11 12:09:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em Yêu';
+const giftUrl = 'https://www.facebook.com/vanah.121?mibextid=ZbWKwL';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng EmPii';
+const contentCard ='Hôm nay là một ngày đặc biệt, là ngày của EmPii! Tui chúc EmPii sinh nhật thật vui vẻ, luôn xinh đẹp, tự tin và mãi là người mà tui yêu thương. Mong rằng năm nay EmPii sẽ có thêm nhiều trải nghiệm tuyệt vời và thành công hơn nữa!';
+const giftImage = 'nen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
